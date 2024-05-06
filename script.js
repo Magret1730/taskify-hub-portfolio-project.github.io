4e668555d13f4e49afa6472ebd93a58bfd82e8ec
Magret <oyedeleabiodun79@gmail.com>
2024-05-06 16:55:04 -0230

    Created Landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll to the target section when clicking on a button
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
